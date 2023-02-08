cc16eb75217b920661867912fcd6f2aead8ba018
Joanna <joanna.malgorzata@gmail.com>
2023-02-08 08:31:13 +0100

    react dom

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import Footer from './components/Footer/Footer'
 import InputDiceType from './components/InputDiceType/InputDiceType';
 import Header from './components/Header/Header';
 import Result from './components/Result/Result';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
-      <InputDiceType />
-      <Result />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Header />
+          <Routes>
+            <Route path="/" element= {<InputDiceType />} />
+            <Route path="/result" element= {<Result />} />
+         </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
