0e097d55643b82ff0f977a3f021bc9b0c7ba248a
Joanna <joanna.malgorzata@gmail.com>
2023-02-09 12:23:20 +0100

    switch drama

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,8 @@ import logoRollADice from '././mycomponents/logoRollADice.gif';
 const Header = () => {
   return (<>
     <Typography variant='h1' fontSize={40}> Roll a Dice</Typography>
-  {/* <img src={logoRollADice} alt='logo'/> */}
+
+  {/* <img src={logoRollADice} /> */}
   </>)
 }
 
--- src/components/InputDiceType/InputDiceType.tsx
+++ src/components/InputDiceType/InputDiceType.tsx
@@ -1,8 +1,10 @@
 import React from 'react' 
-import { TextField, Fab, Box} from '@mui/material';
+import { TextField, Fab, Box, colors} from '@mui/material';
 import CasinoIcon from '@mui/icons-material/Casino';
 import Switch from '@mui/material/Switch';
-
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Typography from '@mui/material';
 
 //Do poprawy
 // const WhitchDice = () => {
@@ -14,25 +16,37 @@ import Switch from '@mui/material/Switch';
 
 
 const InputDiceType = () => {
-    return (<div style={{margin:"10px"}}>
-    <Box>
-  <Switch defaultChecked color="success" /> 
-    <TextField id="outlined-basic" label="?dice" variant="outlined" sx={{mt:2}} />
+    return (<><FormGroup style={{margin:"10px"}}>
+    <Box sx={{mt:2}}>
+  <FormControlLabel sx={{color:'GrayText'}}
+  control={<Switch color="success" />} 
+  label='więcej niż 1 kość'
+  labelPlacement='bottom'
+  disabled={false} /> 
+    <TextField id="outlined-basic" label="?dice" variant="outlined"  />
     </Box>
-    <Box>
- <Switch defaultChecked color="success" /> 
-    <TextField id="outlined-basic" label="dice?" variant="outlined" sx={{mt:2}} />
+    <Box sx={{mt:2}}>
+    <FormControlLabel sx={{color:'GrayText'}}
+  control={<Switch color="success" />} 
+  label='niestandardowa kość'
+  labelPlacement='bottom'
+  disabled={false} /> 
+    <TextField id="outlined-basic" label="dice?" variant="outlined"  />
     </Box>
-    <Box>
-  <Switch defaultChecked color="success" />
-    <TextField id="outlined-basic" label="MOD?" variant="outlined" sx={{mt:2}}/>
+    <Box sx={{mt:2}}>
+    <FormControlLabel sx={{color:'GrayText'}}
+  control={<Switch color="success" />} 
+  label='modyfikatory?'
+  labelPlacement='bottom'
+  disabled={false} /> 
+    <TextField id="outlined-basic" label="MOD?" variant="outlined"/>
     </Box>
-  <br />
-      <Fab size="small" color="secondary" aria-label="add" sx={{margin:2}}>
+  <br /></FormGroup>
+      <Fab size="small" color="secondary" aria-label="add" sx={{margin:2}} >
        <CasinoIcon  />
   {/* onClick={WhitchDice} */}
   </Fab>
-    </div>
+    </>
   )
 }
 
